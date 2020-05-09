3a9a6d577ae1007a1a020ca0d044222832494b6d
Sergei <45260512+SergeyShurkhovetckii@users.noreply.github.com>
2020-05-09 23:03:26 +0200

    master

---
+++ src/blocks/modules/FAQ/FAQ.js
@@ -0,0 +1,13 @@
+modules.define('FAQ', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
