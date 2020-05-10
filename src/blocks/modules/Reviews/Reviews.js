1406f5d2da5db5aca186e6cdbc135a6339f0ede3
Sergei <45260512+SergeyShurkhovetckii@users.noreply.github.com>
2020-05-10 10:16:13 +0200

    master

---
+++ src/blocks/modules/Reviews/Reviews.js
@@ -0,0 +1,13 @@
+modules.define('Reviews', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
