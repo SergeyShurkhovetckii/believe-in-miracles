c7efab6e6f2a57d173ed83d064e35980f8cdd8f0
Sergei <45260512+SergeyShurkhovetckii@users.noreply.github.com>
2020-05-18 14:00:28 +0200

    master

--- src/blocks/modules/sing-page/sing-page.js
+++ src/blocks/modules/sing-page/sing-page.js
@@ -1,4 +1,4 @@
-modules.define('get-help', ['i-bem-dom'], function(provide, bemDom) {
+modules.define('sing-page', ['i-bem-dom'], function(provide, bemDom) {
 
 provide(bemDom.declBlock(this.name, {
     onSetMod: {
