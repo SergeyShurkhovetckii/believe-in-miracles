f5975bc5460657f22a19e6b6160ce1b99dd5251d
Sergei <45260512+SergeyShurkhovetckii@users.noreply.github.com>
2020-06-02 16:33:33 +0200

    master

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -2,7 +2,7 @@ import $ from "jquery";
 
 $(document).ready(function(){
     $('.dropdown-open').click(function(){
-        $('.menu-dropdown').slideToggle(300);      
+        $('.menu-dropdown').slideToggle(500);      
         return false;
     });
 });
