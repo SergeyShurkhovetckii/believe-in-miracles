23be15389317355dc60288a6b4fc003e6b8e68f5
Sergei <45260512+SergeyShurkhovetckii@users.noreply.github.com>
2020-06-03 17:07:18 +0200

    master

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -3,3 +3,4 @@ import "%modules%/footer/footer";
 import "%modules%/front-intro/front-intro";
 import "%modules%/main/main";
 import "%modules%/one-child/one-child";
+import "%modules%/FAQ-page/FAQ-page";
