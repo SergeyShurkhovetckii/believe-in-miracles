1d0039dd9b0da56b1107651b78df37da496747ac
Sergei <45260512+SergeyShurkhovetckii@users.noreply.github.com>
2020-06-04 17:44:29 +0200

    master

--- src/blocks/modules/partners/partners.js
+++ src/blocks/modules/partners/partners.js
@@ -2,7 +2,7 @@ import Swiper from 'swiper';
 
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 3,
-    spaceBetween: 47,
+    spaceBetween: 30,
     slidesPerGroup: 3,
     loop: true,
     loopFillGroupWithBlank: true,
