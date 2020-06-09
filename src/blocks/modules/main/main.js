e257198279a9a1e49465699de64f337312324636
Sergei <45260512+SergeyShurkhovetckii@users.noreply.github.com>
2020-06-09 18:55:00 +0200

    master

--- src/blocks/modules/main/main.js
+++ src/blocks/modules/main/main.js
@@ -1,8 +1,9 @@
 import Swiper from 'swiper';
 
-var swiper = new Swiper('.swiper-container', {
-    navigation: {
-      nextEl: '.swiper-pagination__next',
-      prevEl: '.swiper-pagination__prev',
-    },
-  });
+var swiper = new Swiper('.main-swiper', {
+  slidesPerView:1,
+  navigation: {
+    nextEl: '.main-swiper__next',
+    prevEl: '.main-swiper__prev',
+  },
+});
