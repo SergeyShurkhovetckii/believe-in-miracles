621ed6ba073c6b25108923be0e38237a64f0e3d3
Sergei <45260512+SergeyShurkhovetckii@users.noreply.github.com>
2020-06-12 16:39:27 +0200

    master

--- src/blocks/modules/programa/programa.js
+++ src/blocks/modules/programa/programa.js
@@ -1,12 +1,27 @@
 import Swiper from 'swiper';
 
-var swiper = new Swiper('.programa__row', {
+var programa = new Swiper('.programa__row', {
+    direction: 'horizontal',
     slidesPerView:3,
+    slideVisibleClass: 'none',
     spaceBetween:40,
-    width:245,
     centeredSlides: false,
     navigation: {
       nextEl: '.programa__next',
       prevEl: '.programa__prev',
     },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      680: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      820: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+    }
   });
