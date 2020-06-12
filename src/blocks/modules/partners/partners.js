eae5a046ab419efb4fa1a10dd297ea393d2d559c
Sergei <45260512+SergeyShurkhovetckii@users.noreply.github.com>
2020-06-12 17:32:41 +0200

    master

--- src/blocks/modules/partners/partners.js
+++ src/blocks/modules/partners/partners.js
@@ -2,7 +2,7 @@ import Swiper from 'swiper';
 
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 3,
-    spaceBetween: 30,
+    spaceBetween: 40,
     slidesPerGroup: 3,
     loop: true,
     loopFillGroupWithBlank: true,
@@ -11,4 +11,18 @@ var swiper = new Swiper('.swiper-container', {
       nextEl: '.parther-swiper__next',
       prevEl: '.parther-swiper__prev',
     },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      680: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      820: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+    }
   });
